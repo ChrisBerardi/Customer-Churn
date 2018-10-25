bc7fe8351183fdca4ad32caf0e5f42b5968f0917
Chris Berardi <33790068+ChrisBerardi@users.noreply.github.com>
2018-10-25 13:55:26 -0700

    Update churn.py

--- churn.py
+++ churn.py
@@ -19,6 +19,46 @@ from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import cross_validate
 from sklearn.model_selection import train_test_split
 
+from imblearn.under_sampling import RandomUnderSampler
+
+# Function for calculating loss and confusion matrix
+def loss_cal(y, y_predict, fp_cost, fn_cost, tp_cost, display=True):
+    loss     = [0, 0, 0, 0]       #False Neg Cost, False Pos Cost, 
+                                  #True Neg Cost, True positive savings
+    conf_mat = [0, 0, 0, 0] #tn, fp, fn, tp
+    for j in range(len(y)):
+        if y[j]==0:
+            if y_predict[j]==0:
+                conf_mat[0] += 1 #True Negative
+                loss[2] += 1
+            else:
+                conf_mat[1] += 1 #False Positive
+                loss[1] += fp_cost[j]
+        else:
+            if y_predict[j]==1:
+                conf_mat[3] += 1 #True Positive
+                loss[3] += fn_cost[j]
+            else:
+                conf_mat[2] += 1 #False Negative
+                loss[0] += fn_cost[j]
+    if display:
+        fn_loss = loss[0]
+        fp_loss = loss[1]
+        tp_loss = loss[2]
+        tp_save = loss[3]
+        total_loss = fn_loss + fp_loss + tp_loss
+        total_saved = tp_save-total_loss
+        misc    = conf_mat[1] + conf_mat[2]
+        misc    = misc/len(y)
+        print("{:.<23s}{:10.4f}".format("Misclassification Rate", misc))
+        print("{:.<23s}{:10.0f}".format("False Negative Cost", fn_loss))
+        print("{:.<23s}{:10.0f}".format("False Positive Cost", fp_loss))
+        print("{:.<23s}{:10.0f}".format("Total Loss", total_loss))
+        print("{:.<23s}{:10.0f}".format("Total Saved", total_saved))
+    return loss, conf_mat
+
+
+
 file_path = "C:/Users/Saistout/Desktop/data/customer churn/"
 df = pd.read_csv(file_path + 'WA_Fn-UseC_-Telco-Customer-Churn.csv')
 
@@ -32,6 +72,10 @@ print(df.describe(include='all'))
 
 for i in (0, len(df.columns)-1):
     print(df.iloc[:,i].value_counts())
+    
+#Calculate total amount lost to churn
+index = df['Churn'] == 'Yes'
+total_churn = df['MonthlyCharges'][index].sum()
 
 #Change all null values for TotalCharges to zero to indicate they haven't
 #had any yet.
@@ -198,4 +242,78 @@ DecisionTree.display_binary_split_metrics(dtc, X_train_t, y_train_t, \
                                      X_validate_t, y_validate_t)
 
 #Huge false positive rate! We are missing most of the churn, need to move on to
-#lose function to have any hope of modeling this data
+#lose and retained functiosn to have any hope of modeling this data
+#Lose will be the montly payments lost i.e. churn not predicted plus the cost
+#of preventing churn i.e. cost per true negative or false negative
+#Retained will be the monthly payments for true negatives i.e. payments from
+#churn prevented 
+
+# Setup false positive and false negative costs for each transaction
+ #Make a nominal cost for a fale positive i.e. they get thing even though
+ #They won't churn
+fp_cost = np.array(df['MonthlyCharges']*0+20)
+#False negatives cost the full amount i.e. they churn and we lose them
+fn_cost = np.array(df['MonthlyCharges']) 
+#Nominal true positive cost i.e. cost to run program to stop churn for 
+#those that will
+tp_cost = np.array(0*df['MonthlyCharges']+20)
+
+# Setup random number seeds
+rand_val = np.array([1, 15, 168, 5156, 71686])
+# Ratios of Majority:Minority Events
+ratio = ['70:30','40:60', '50:50', '60:40', '70:30']
+# Dictionaries contains number of minority and majority events in each ratio sample
+# n_majority = ratio x n_minority
+rus_ratio = ({0:801,1:1869},{0:1246,1:1869},{0:1869, 1:1869}, {0:2804, 1:1869}, \
+             {0:4361, 1:1869})
+
+# Best model is one that maximizes savings
+min_saved   = -9e+15
+best_ratio = 0
+for k in range(len(rus_ratio)):
+    rand_vals = (k+1)*rand_val
+    print("\nDecision Tree Model using " + ratio[k] + " RUS")
+    fn_loss  = np.zeros(len(rand_vals))
+    fp_loss  = np.zeros(len(rand_vals))
+    misc     = np.zeros(len(rand_vals))
+    tp_loss  = np.zeros(len(rand_vals))
+    fn_saved = np.zeros(len(rand_vals))
+    for i in range(len(rand_vals)):
+        rus = RandomUnderSampler(ratio=rus_ratio[k], \
+                random_state=rand_vals[i], return_indices=False, \
+                replacement=False)
+        X_rus, y_rus = rus.fit_sample(X_t, np_y_t)
+        tr = DecisionTreeClassifier(max_depth=8, min_samples_leaf=5, \
+                                 min_samples_split=5,criterion='gini')
+        tr.fit(X_rus, y_rus)
+        loss, conf_mat = loss_cal(np_y_t, tr.predict(X_t), fp_cost, fn_cost,\
+                                  tp_cost,display=False)
+        fn_loss[i]  = loss[0]
+        fp_loss[i]  = loss[1]
+        tp_loss[i]  = loss[2]
+        fn_saved[i] = loss[3]
+        misc[i]    = conf_mat[1] + conf_mat[2]
+    misc = np.sum(misc)/(len(df) * len(rand_vals))
+    fn_avg_loss = np.average(fn_loss)
+    fp_avg_loss = np.average(fp_loss)
+    tp_avg_loss = np.average(tp_loss)
+    avg_saved   = np.average(fn_saved)
+    total_loss  = fn_loss + fp_loss + tp_loss
+    avg_loss    = np.average(total_loss)
+    std_loss    = np.std(total_loss)
+    total_saved = fn_saved - total_loss
+    net_saved   = np.average(total_saved)
+    std_saved   = np.std(total_saved)
+
+    print("{:.<23s}{:10.4f}".format("Misclassification Rate", misc))
+    print("{:.<23s}{:10.0f}".format("False Negative Cost", fn_avg_loss))
+    print("{:.<23s}{:10.0f}".format("False Positive Cost", fp_avg_loss))
+    print("{:.<23s}{:10.0f}".format("True Positive Cost", tp_avg_loss))
+    print("{:.<23s}{:10.0f}".format("True Positive Savings", avg_saved))
+    print("{:.<23s}{:10.0f}{:5s}{:<10.2f}".format("Total Loss", avg_loss, \
+                  " +/- ", std_loss))
+    print("{:.<23s}{:10.0f}{:5s}{:<10.2f}".format("Net Saved", net_saved, \
+                  " +/- ", std_saved))
+    if net_saved > min_saved:
+        min_saved   = net_saved
+        best_ratio = k
